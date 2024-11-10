63058f54d5fbaf36024e6666ef91f31e037f077c
BartekR35 <bartosz.rainka98@gmail.com>
2024-11-10 16:16:40 +0100

    First commit

---
+++ src/TaxCounting.java
@@ -0,0 +1,25 @@
+// Product net cost is 9.99. Tax is 23%. Multiply it by 10000 (like you sell it). Then count net cost of all.
+// Then do the same but with th Big Decimal class
+
+public class TaxCounting {
+    public static void main(String[] args) {
+
+        /* double P1NetCostRainka = 9.99;
+        double P1GrossCostRainka = P1NetCostRainka * 1.23;
+
+        P1GrossCostRainka = P1GrossCostRainka * 10000;
+
+        P1NetCostRainka = P1GrossCostRainka * 0.77;
+
+        System.out.println(P1NetCostRainka);
+
+        */
+
+        // same with BigDecimal class
+
+
+
+
+
+    }
+}
---
+++ src/WhatsYourWeightOnMars.java
@@ -0,0 +1,46 @@
+// Mars gravity is about 38% of Earth's. Write a program which will count Your weight on Mars
+// Plus variable type conversions
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class WhatsYourWeightOnMars {
+    public static void main(String[] args) {
+      float EarthWRainka = 100;
+      float MarsWRainka = (float) (EarthWRainka * 0.38);
+
+      System.out.println(MarsWRainka);
+
+      // some data type conversions below
+
+      double RandomVarRainka = 1.31415925;
+
+      //double -> BigDecimal and limit to 4 zeros after coma
+
+      BigDecimal RoundedRandomVarRainka = BigDecimal.valueOf(RandomVarRainka).setScale(4, RoundingMode.HALF_UP);
+
+      System.out.println(RoundedRandomVarRainka);
+
+      // double -> int
+
+      double SomeDigitRainka = 63.340;
+
+      SomeDigitRainka = (int) SomeDigitRainka;;
+
+      // int -> char
+
+      SomeDigitRainka = (char) SomeDigitRainka;
+
+      System.out.println(SomeDigitRainka);;
+
+      //Some math on variable
+
+      SomeDigitRainka = (double) (SomeDigitRainka);
+
+      SomeDigitRainka = SomeDigitRainka * 5.4;
+
+      System.out.println(SomeDigitRainka);
+
+
+    }
+}
